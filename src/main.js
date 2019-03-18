bf1ef85dbbeeabc8071cf8446346326ca16c8896
RenYongqiang <remonweb@yeah.net>
2019-03-18 14:00:02 +0800

    style

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import MintUI from 'mint-ui';
 import 'mint-ui/lib/style.css';
+import '@/assets/css/reset.css'
+import '@/assets/css/common.css'
 import router from './router'
 // import axios from 'axios'
  
@@ -16,4 +18,21 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
+//兼容性：字体大小，全局尺寸(rem)
+(function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      if (clientWidth >= 750) {
+        docEl.style.fontSize = '20px';
+      } else {
+        docEl.style.fontSize = 24 * (clientWidth / 750) + 'px';
+      }
+    };
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window)

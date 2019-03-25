5f59dd325e5618536db2260743502787aa1f78b8
RenYongqiang <remonweb@yeah.net>
2019-03-25 17:36:54 +0800

    create two

---
+++ src/views/mine/mine.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- 头部 -->
+        <div class="mineHeaderMain">
+            <h3>个人中心</h3>            
+            <div>
+                
+            </div>
+        </div> 
+        <div class="mineBodyMain">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>

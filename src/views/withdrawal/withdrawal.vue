981ce55810fe3a9874d20ce62f802d0dbd03a1da
angelingRemon <790223383@qq.com>
2019-08-12 16:33:37 +0800

    提现验证码

--- src/views/withdrawal/withdrawal.vue
+++ src/views/withdrawal/withdrawal.vue
@@ -556,6 +556,7 @@ export default {
     bottom: 0;
     right: 0;
     background: rgba(0, 0, 0, 0.5);
+    z-index:999;
 }
 .puponVerifyMain {
     position: fixed;
@@ -568,6 +569,7 @@ export default {
     box-sizing: border-box;
     padding-bottom:0.4rem;
     border-radius:0.1rem;
+    z-index:9999;
     .closeBtn{
         width:0.4rem;
         height:0.4rem;

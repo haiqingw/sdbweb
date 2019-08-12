4f3bac996a944dd52619c7da8770eeb866f1f524
angelingRemon <790223383@qq.com>
2019-08-12 11:55:09 +0800

    permission

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,6 @@
 import router from './router'
 import store from './store'
-const whiteList = ['/login', '/forgetPassword', '/forgetPasswordNext', '/loginoid', '/accountNumber-cancellation', '/smsVerification']
+const whiteList = ['/login', '/forgetPassword', '/forgetPasswordNext', '/loginoid', '/accountNumber-cancellation']
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
